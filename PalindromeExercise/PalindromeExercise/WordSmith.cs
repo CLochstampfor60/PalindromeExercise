8546abbbc950f3c314585940fe6a0747434cc0bb
CLochstampfor60 <cflochjr@gmail.com>
2024-01-09 21:37:11 -0500

    complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string myWord)
+        {
+            if (myWord.SequenceEqual(myWord.Reverse()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,41 @@
-using System;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("buttmonkey", false)]
+        [InlineData("civic", true)]
+        [InlineData("arbok", false)]
+        [InlineData("kayak", true)]
+        [InlineData("ekans", false)]
+        [InlineData("deified", true)]
+        [InlineData("deed", true)]
+        [InlineData("dad", true)]
+        [InlineData("grandma", false)]
+        [InlineData("grandpa", false)]
 
+        public void IsAPalindrome(string myWord, bool expected)
+        {
+            // Arrange
+            var ws = new WordSmith();
+
+            // Actual
+            var actual = ws.IsAPalindrome(myWord);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
+
+
 }
